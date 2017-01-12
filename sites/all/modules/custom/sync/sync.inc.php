c594b0fdcba197b73e8c48020061acfdb172f5c6
User Greenma2 <greenma2@qs2991.pair.com>
2017-01-11 23:39:08 -0500

    Adding the Open Green Map website to Git instead of the old SVN repo. The full commit history before this is available at https://github.com/greenmap/ogm-archived

---
+++ sites/all/modules/custom/sync/sync.inc.php
@@ -0,0 +1,11 @@
+<?php
+// this file not stored in svn
+$db_user = 'greenma2';
+$db_pass = 'q2n7yGEm';
+$db_name = 'greenma2_greenhouse';
+
+function sync_set_db_authentication(&$db_user, &$db_pass, &$db_name) {
+  $db_user = 'greenma2';
+  $db_pass = 'q2n7yGEm';
+  $db_name = 'greenma2_greenhouse';
+}
